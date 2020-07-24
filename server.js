307884f74533164bc9c51aa1f4ceb6ec77766621
Barbs <barbarap095@gmail.com>
2020-07-24 20:02:42 +1000

    Add routing for get and post

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 // Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const noteData = require("./Develop/db/db.json")
+
 
 // Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 2104;
+
+const app = express();
+const PORT = process.env.PORT || 2104;
 
 // Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Route for css page
+
 app.use(express.static("Develop/public/assets/css"));
 
+// HTML ROUTES//
 // Route for index page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
@@ -25,13 +31,29 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
+// API ROUTES//
+// Route for getting note data
+app.get("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, noteData), function(err,data) {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        console.log(notes);
+        res.json(notes);
+    });
+});
 
+// Route for adding new note via JSON, and displaying it
+app.post("/api/notes", function(req, res) {
+    const newNotes = req.body;
+    console.log(newNotes);
 
+    noteData.push(newNotes);
+    res.json(newNotes);
 
-
+});
 
 // Starts the server to begin listening
-// =============================================================
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   }); 
