935d156605b5b1b30ab8291b3f98d6ac509dc934
Barbs <barbarap095@gmail.com>
2020-07-27 22:30:22 +1000

    Edit POST route

--- server.js
+++ server.js
@@ -21,40 +21,47 @@ app.use(express.static("Develop/public"));
 
 // HTML ROUTES//
 // Route for index page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 // Route for notes page
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 // API ROUTES//
 // GET route for getting note data
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
     fs.readFile(path.join(__dirname, "./Develop/db/db.json"));
 });
 
 // POST route for adding new notes via JSON, and displaying it
-app.post("/api/notes", function(req, res) {
-    const newNotes = {
-        id: noteData.length + 1,
-        title: noteData.title,
-        text: noteData.text
-    }
-    console.log(newNotes);
-    noteData.push(newNotes);
+app.post("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "./Develop/db/db.json"), "utf8", function (err, res) {
+        if (err) {
+            console.log(err);
+        }
+        const notes = JSON.parse(res);
+        const noteReq = req.body;
 
-    fs.writeFile(path.join(__dirname, "./Develop/db/db.json", JSON.stringify(notes)), function(err) {
-        if (err) throw err;
-        return res.json(notes)
+        const newNotes = {
+            id: notes.length + 1,
+            title: noteReq.title,
+            text: noteReq.text
+        };
+        res.json(newNotes);
+        noteData.push(newNotes);
+
+        fs.writeFile(path.join(__dirname, "./Develop/db/db.json", JSON.stringify(notes, null, 2)), function (err) {
+            if (err) throw err;
+            return res.json(notes)
+        });
     });
 });
-
-// Route for deleting notes 
-app.delete("/api/notes/:id", function(req, res) {
-    fs.readFile(path.join(__dirname, "./Develop/db/db.json"), function(err,data) {
+    // Route for deleting notes 
+app.delete("/api/notes/:id", function (req, res) {
+    fs.readFile(path.join(__dirname, "./Develop/db/db.json"), function (err, data) {
         if (err) throw err;
         const savedNotes = JSON.parse(data);
         console.log(savedNotes);
@@ -65,6 +72,6 @@ app.delete("/api/notes/:id", function(req, res) {
 
 // Starts the server to begin listening
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  }); 
+}); 
